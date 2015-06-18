12dd86f72f97aa7db8ec370a759e32c01c25e7c6
schrist81 <schrist81@users.noreply.github.com>
2015-06-18 10:08:32 +0100

    PeakDetection and SpikeFrequency added

--- BasicPropertyAnalzyer.py
+++ BasicPropertyAnalzyer.py
@@ -10,6 +10,13 @@ from openpyxl.compat import range
 from openpyxl.cell import get_column_letter
 from openpyxl.styles import Font, Fill
 
+# Imports for Peak Detection
+import numpy as np
+from math import pi, log
+import pylab
+from scipy import fft, ifft
+from scipy.optimize import curve_fit
+
 rec = stfio.read("14122000.abf")
 last_value = 0
 complete_dataset = np.array([0,0])
@@ -49,7 +56,13 @@ ws1['A24'] = "iAP code"
 
 ws1['A27'] = "filename"
 ws1['A28'] = "sweep number"
-ws1['A36'] = "Spike frequency (0.1 s)"
+
+ws1['A30'] = "Overshoot (mv)"
+
+
+
+
+ws1['A36'] = "Spike frequency (1 s)"
 ws1['A37'] = "-10"
 ws1['A38'] = "0"
 ws1['A39'] = "10"
@@ -72,6 +85,124 @@ ws1['A55'] = "170"
 ws1['A56'] = "180"
 dest_filename = 'F:\Programmierung\Python\empty_book.xlsx'
 wb.save(filename = dest_filename)
+
+
+def _datacheck_peakdetect(x_axis, y_axis):
+    if x_axis is None:
+        x_axis = range(len(y_axis))
+    
+    if len(y_axis) != len(x_axis):
+        raise (ValueError, 
+                'Input vectors y_axis and x_axis must have same length')
+    
+    #needs to be a numpy array
+    y_axis = np.array(y_axis)
+    x_axis = np.array(x_axis)
+    return x_axis, y_axis
+
+
+def peakdetect(y_axis, x_axis = None, lookahead = 100, delta=0):
+    """
+    Converted from/based on a MATLAB script at: 
+    http://billauer.co.il/peakdet.html
+    found at https://gist.github.com/sixtenbe/1178136
+    
+    function for detecting local maximas and minmias in a signal.
+    Discovers peaks by searching for values which are surrounded by lower
+    or larger values for maximas and minimas respectively
+    
+    keyword arguments:
+    y_axis -- A list containg the signal over which to find peaks
+    x_axis -- (optional) A x-axis whose values correspond to the y_axis list
+        and is used in the return to specify the postion of the peaks. If
+        omitted an index of the y_axis is used. (default: None)
+    lookahead -- (optional) distance to look ahead from a peak candidate to
+        determine if it is the actual peak (default: 200) 
+        '(sample / period) / f' where '4 >= f >= 1.25' might be a good value
+    delta -- (optional) this specifies a minimum difference between a peak and
+        the following points, before a peak may be considered a peak. Useful
+        to hinder the function from picking up false peaks towards to end of
+        the signal. To work well delta should be set to delta >= RMSnoise * 5.
+        (default: 0)
+            delta function causes a 20% decrease in speed, when omitted
+            Correctly used it can double the speed of the function
+    
+    return -- two lists [max_peaks, min_peaks] containing the positive and
+        negative peaks respectively. Each cell of the lists contains a tupple
+        of: (position, peak_value) 
+        to get the average peak value do: np.mean(max_peaks, 0)[1] on the
+        results to unpack one of the lists into x, y coordinates do: 
+        x, y = zip(*tab)
+    """
+    max_peaks = []
+    min_peaks = []
+    dump = []   #Used to pop the first hit which almost always is false
+       
+    # check input data
+    x_axis, y_axis = _datacheck_peakdetect(x_axis, y_axis)
+    # store data length for later use
+    length = len(y_axis)
+    
+    
+    #perform some checks
+    if lookahead < 1:
+        raise ValueError, "Lookahead must be '1' or above in value"
+    if not (np.isscalar(delta) and delta >= 0):
+        raise ValueError, "delta must be a positive number"
+    
+    #maxima and minima candidates are temporarily stored in
+    #mx and mn respectively
+    mn, mx = np.Inf, -np.Inf
+    
+    #Only detect peak if there is 'lookahead' amount of points after it
+    for index, (x, y) in enumerate(zip(x_axis[:-lookahead], 
+                                        y_axis[:-lookahead])):
+        if y > mx:
+            mx = y
+            mxpos = x
+        if y < mn:
+            mn = y
+            mnpos = x
+        
+        ####look for max####
+        if y < mx-delta and mx != np.Inf:
+            #Maxima peak candidate found
+            #look ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead].max() < mx:
+                if y_axis[mxpos] > 0: 
+                    max_peaks.append([mxpos, mx])
+                    dump.append(True)
+                #set algorithm to only find minima now
+                mx = np.Inf
+                mn = np.Inf
+                if index+lookahead >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
+                continue
+            #else:  #slows shit down this does
+            #    mx = ahead
+            #    mxpos = x_axis[np.where(y_axis[index:index+lookahead]==mx)]
+        
+        ####look for min####
+        
+        if y > mn+delta and mn != -np.Inf:
+            #Minima peak candidate found 
+            #look ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead].min() > mn:
+                min_peaks.append([mnpos, mn])
+                dump.append(False)
+                #set algorithm to only find maxima now
+                mn = -np.Inf
+                mx = -np.Inf
+                if index+lookahead >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
+            #else:  #slows shit down this does
+            #    mn = ahead
+            #    mnpos = x_axis[np.where(y_axis[index:index+lookahead]==mn)]
+    return max_peaks    
+
+
 
 class spontActivity():
     def attempted_action_potential_found(complete_section):
@@ -175,6 +306,9 @@ class Example(Frame):
         
 
             i = 0
+            #set sweep number to -1 to set it later to the first sweep with an AP
+            sweep = -1
+            print sweep
             while i < len(rec[0]):
                 
                 trace = rec[0][i].asarray()
@@ -187,17 +321,35 @@ class Example(Frame):
                 if (i == 0):
                     if (trace[trace.argmax()] < 0):
                         first_mean = trace[interval_begin:interval_end].mean()
-                        print "First mean: " + str(first_mean)
                     else:
-                        print "Error"
+                        first_mean = "Error"
                 elif (i == 1): 
                     if (trace[trace.argmax()] < 0):
                         second_mean = trace[interval_begin:interval_end].mean()
-                        print "Second mean: " + str(second_mean)
                     else:
-                        print "Error"
+                        second_mean = "Error"
                 i = i + 1
+                
+
+                #extracts the 1s current step part of each sweep
+                injected_trace = trace[1612:101612]                 
+                coordinate = "B" + str(36+i)
+                if (injected_trace[injected_trace.argmax()] > 0):
+                    if sweep == -1:
+                        ws1['B28'] = i
+                        sweep = i
+                        # Determine Overshoot
+                        print peakdetect(injected_trace, None, lookahead = 300, delta=0)[0][1]
+                        #ws1['B30'] = 
+                    iAP_frequency = len(peakdetect(injected_trace, None, lookahead = 300, delta=0))
+                    ws1[coordinate] = iAP_frequency
+                else:
+                    ws1[coordinate] = 0
             ws1['B8'] = inducedActivity().calculateInputResistance(first_mean, second_mean)  
+            ws1['B27'] = iAP_file
+            wb.save(filename = dest_filename)
+            
+    
         
     def onOpenGapFree(self):
         global rec, complete_dataset, abffile
@@ -224,7 +376,7 @@ class Example(Frame):
             number_of_sections = len(rec[0])
   
             # Alle Section aus Channel 0 in eine Datenspur vereinen
-            # Needs improvement, very slow, possibly too slow for comfortable working with it
+            # Needs improvement, very slow, definetely too slow for comfortable working with it
             i = 1
             while i < number_of_sections:
                 helper_arr = rec[0][i].asarray()    
@@ -253,10 +405,10 @@ class Example(Frame):
                     ws1['B14'] = ''
                     ws1['B15'] = ''
                     #RMP only determined in sAP code 1 files (other cases more complicated, maybe later)
-                    ws1['B7'] = spontActiviy().mean_first_10s(complete_dataset)   
+                    ws1['B7'] = spontActivity().mean_first_10s(complete_dataset)   
 
             ws1['B16'] = sAP
-            ws1['B6'] =  rig
+            ws1['B6'] = rig
             ws1['B5'] = abffile
             wb.save(filename = dest_filename)
     def readFile(self, filename):
